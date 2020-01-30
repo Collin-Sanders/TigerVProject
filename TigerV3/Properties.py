bb6283ec5d9e40de6a5ef43cac421e3a32c2c573
Collin Sanders <54324382+Collin-Sanders@users.noreply.github.com>
2020-01-30 11:22:33 -0500

    Add files via upload

---
+++ TigerV3/Properties.py
@@ -0,0 +1,18 @@
+button_text = ['Fog Lights', 'Light Bar', 'Winch',
+ 'Air Comp', 'Bed Lights', 'Rock Lights']
+image_names_off = [
+ 'car-foglight-off.png', 'car-lightbar-off.png', 'car-bedlights-off.png', 'car-rocklights-off.png', 'car-winch-off.png', 'car-aircompressor-off.png']
+image_names_on = ['car-foglight-on.png', 'car-lightbar-on.png', 'car-bedlights-on.png', 'car-rocklights-on.png', 'car-winch-on.png', 'car-aircompressor-on.png']
+strobe_images = ['car-foglight-strobe.png', 'car-lightbar-strobe.png', 'car-bedlights-strobe.png', 'car-rocklights-strobe.png']
+screen_width = 480
+screen_height = 320
+default_bg_color = 'black'
+page_button_width = 8
+page_button_height = 3
+page_text_size = 21
+on_text_color = 'green'
+off_text_color = 'red'
+strobe_text_color = 'blue'
+back_text_color = 'white'
+button_width = int(screen_width / 3) - 6
+button_height = int(screen_height / 2) - 6
